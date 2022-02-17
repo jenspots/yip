c00800974e17264807976385c686e06737c80218
Jens Pots <jens@jenspots.com>
2022-02-17 20:39:44 +0100

    Properly spawn a given amount of threads.

--- whatsmyip.c
+++ whatsmyip.c
@@ -12,10 +12,17 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <pthread.h>
+#include <stdnoreturn.h>
 
 #define PORT 80
+#define THREAD_COUNT 10
 
-void* handle_request(void * arg)
+/**
+ * Function that handles incoming requests until the end of time, or when the
+ * program halts, whichever comes first.
+ * @param arg A pointer to the socket_identifier received in `main`.
+ */
+noreturn void* handle_request(void * arg)
 {
     int socket_identifier, handler;
     char *ip;
@@ -26,12 +33,14 @@ void* handle_request(void * arg)
     socket_identifier = * (int*) arg;
 
     /* Respond with the address and close the handling socket. */
-    handler = accept(socket_identifier, (struct sockaddr *) &client, &address_len);
-    ip = inet_ntoa(client.sin_addr);
-    write(handler, "HTTP/1.1 200 OK\n\n", 16);
-    write(handler, "Content-length: 9\n\n", 19);
-    write(handler, ip, strlen(ip));
-    close(handler);
+    while (1) {
+        handler = accept(socket_identifier, (struct sockaddr *) &client, &address_len);
+        ip = inet_ntoa(client.sin_addr);
+        write(handler, "HTTP/1.1 200 OK\n\n", 16);
+        write(handler, "Content-length: 9\n\n", 19);
+        write(handler, ip, strlen(ip));
+        close(handler);
+    }
 }
 
 int main(int argc, char ** argv)
@@ -50,9 +59,12 @@ int main(int argc, char ** argv)
     bind(socket_identifier, (const struct sockaddr *) &server, sizeof(server));
     listen(socket_identifier, 1);
 
-    /* Infinitely respond with the IP address. */
-    while (1) {
+    /* Spawn worker threads. */
+    for (int i = 0; i < THREAD_COUNT; ++i) {
         pthread_create(&thread_id, NULL, handle_request, &socket_identifier);
-        pthread_join(thread_id, NULL);
     }
+
+    /* If the main thread exits, the program exits as a whole. Instead of
+     * yielding the main thread, we might as well do something with it. */
+    handle_request(&socket_identifier);
 }
