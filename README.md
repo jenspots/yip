6c9d8285077124de5a9d110aa1bd0a8aa14e78f5
Jens Pots <jens@jenspots.com>
2022-02-17 21:27:28 +0100

    Argument parsers. Updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# What's My IP?
+# What's my IP address?
 
-Performant, lightweight web server that echos the client's IP address.
+### About
 
+In here you'll find a high performance, multithreaded HTTP server that only knows how to do one thing: return the client's IP address. It is extremely lightweight, and should be able to handle and immense amount of traffic when completed.
+
+```
+OPTIONS
+  -h --help             Display this help page.
+  -c --count <u_int>    Specify number of threads.
+  -v --verbose          Be more verbose.
+  -l --log              Log addresses to stdout.
+```
+
+### Motivation
+
+I've grown tired of Googling "what's my IP address" since I don't like to rely on a GUI for such a trivial task, and I simply am unable to remember the domain name of that one service that works with an easy `cURL` query. Given how small the implementation would be, I figured it would be fun to try and write a C program using `pthread.h` and `socket.h`.
+
+### A work in progress
+
+Some things that still need to be done.
+
+- [ ] Error handling
+- [ ] Code cleanup
+- [ ] Make the UX a bit nicer
--- whatsmyip.c
+++ whatsmyip.c
@@ -13,24 +13,49 @@
 #include <string.h>
 #include <pthread.h>
 #include <stdnoreturn.h>
+#include <getopt.h>
+#include <stdio.h>
+
+/* Style codes to format terminal output. */
+#define ANSI_STYLE_BOLD    "\033[1m"
+#define ANSI_STYLE_RESET   "\033[22m"
+#define BOLD(x) ANSI_STYLE_BOLD""x""ANSI_STYLE_RESET
 
 #define PORT 80
-#define THREAD_COUNT 10
+
+/* Global flag set by the user at runtime. */
+static int verbose_flag = 0;
+static int log_flag = 0;
+
+/* Message displayed as help page. */
+static char* help_message =
+        "Lightweight, multi-threaded web server which echoes the client's IP address.\n"
+        "Copyright (c) by Jens Pots\n"
+        "Licensed under AGPL-3.0-only\n"
+        "\n"
+        BOLD("OPTIONS\n")
+        "  -h --help\t\tDisplay this help page.\n"
+        "  -c --count <u_int>\tSpecify number of threads.\n"
+        "  -v --verbose\t\tBe more verbose.\n"
+        "  -l --log\t\tLog addresses to stdout.\n";
 
 /**
  * Function that handles incoming requests until the end of time, or when the
  * program halts, whichever comes first.
  * @param arg A pointer to the socket_identifier received in `main`.
+ * @warning Never returns.
  */
 noreturn void* handle_request(void * arg)
 {
     int socket_identifier, handler;
     char *ip;
     struct sockaddr_in client;
-    socklen_t address_len = sizeof(client);
 
     /* The void pointer contains the original socket number. */
     socket_identifier = * (int*) arg;
+
+    /* Required by `accept`. */
+    socklen_t address_len = sizeof(client);
 
     /* Respond with the address and close the handling socket. */
     while (1) {
@@ -40,14 +65,49 @@ noreturn void* handle_request(void * arg)
         write(handler, "Content-length: 9\n\n", 19);
         write(handler, ip, strlen(ip));
         close(handler);
+
+        if (log_flag) {
+            printf("Served client: %s\n", ip);
+        }
     }
 }
 
 int main(int argc, char ** argv)
 {
-    int socket_identifier;
+    int socket_identifier, thread_count;
     struct sockaddr_in server;
     pthread_t thread_id;
+
+    /* These values define which switches are legal. */
+    char options[] = "c:h";
+    struct option long_options[] = {
+        {"verbose", no_argument,       &verbose_flag, 1  },
+        {"count",   required_argument, NULL,          'c'},
+        {"log",     no_argument,       &log_flag,     1  },
+        {NULL,      0,                 NULL,          0  }
+    };
+
+    /* Parse runtime arguments. */
+    while (1) {
+        switch (getopt_long(argc, argv, options, long_options, NULL)) {
+            case -1:
+                goto exit_while_loop;
+            case 'c':
+                thread_count = atoi(optarg);
+                printf("Assigning %d worker threads\n", thread_count);
+                break;
+            case 'l':
+                printf("Logging users\n");
+                break;
+            case 'h':
+                printf("%s", help_message);
+                exit(-1);
+            case '?':
+                printf("Unknown parameter\n");
+                exit(-1);
+        };
+    }
+    exit_while_loop:
 
     /* Configure the socket. */
     server.sin_family = AF_INET;
@@ -59,8 +119,8 @@ int main(int argc, char ** argv)
     bind(socket_identifier, (const struct sockaddr *) &server, sizeof(server));
     listen(socket_identifier, 1);
 
-    /* Spawn worker threads. */
-    for (int i = 0; i < THREAD_COUNT; ++i) {
+    /* Spawn worker threads. The current thread is the zeroth, so i = 1. */
+    for (int i = 1; i < thread_count; ++i) {
         pthread_create(&thread_id, NULL, handle_request, &socket_identifier);
     }
 
